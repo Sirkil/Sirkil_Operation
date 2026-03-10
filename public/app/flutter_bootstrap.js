d94bb941652ed5d894b4c0c54005bab78921ecd1
Sirkil <tanany.sirkil@gmail.com>
2026-03-10 10:44:02 +0200

    Add files via upload

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "588484005"
+    serviceWorkerVersion: "2119801661"
   }
 });
